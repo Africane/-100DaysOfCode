18008d49a200812c36cb0dc48052bbe5eb5fff3b
David Owino <daveowino753@gmail.com>
2024-09-03 04:31:41 +0300

    Removed boiler plate code. Added src dir and screens dir in said dir

---
+++ Day 2/login_bloc/lib/src/screens.dart/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
