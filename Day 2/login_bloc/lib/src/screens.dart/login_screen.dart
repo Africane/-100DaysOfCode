c51d4d279b6a10d4055a737bf985f6c161a4c53b
David Owino <daveowino753@gmail.com>
2024-09-03 05:04:22 +0300

    added blocs dir and bloc.dart file in said dir

--- Day 2/login_bloc/lib/src/screens.dart/login_screen.dart
+++ Day 2/login_bloc/lib/src/screens.dart/login_screen.dart
@@ -5,12 +5,52 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          
-        ],
+    return SafeArea(
+      child: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            emailField(),
+            passwordField(),
+            Container(margin: const EdgeInsets.only(top: 25.0),),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return const TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'Email Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return const TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Enter Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      onPressed: () {
+        print('1');
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+      ), 
+      child: const Text(
+        'Login',
+        style: TextStyle(color: Colors.white),
       ),
     );
   }
