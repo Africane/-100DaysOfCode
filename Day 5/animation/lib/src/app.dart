8575367bd0caf7cf6e444f812ada0fdb30953ec0
David Owino <daveowino753@gmail.com>
2024-09-09 08:46:28 +0300

    updated all the dirs

--- Day 5/animation/lib/src/app.dart
+++ Day 5/animation/lib/src/app.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
       title: 'Animation',
       theme: ThemeData(
         primarySwatch: Colors.blue),
-      home: Home(),
+      home: const Home(),
     );
   }
 }
